d65ff8d4dcd0fe5ce6af835c8f989b5da1a3b5e3
KseniaStrizh <kseniya.blinnicheva@gmail.com>
2017-12-01 18:34:12 +0300

    Added ex1 for lessons 2

---
+++ Lesson2_Ex1/Lesson2_Ex1/UnitTest1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Threading;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+
+namespace Lesson2_Ex1
+{
+    [TestClass]
+    public class OpenPageAndCloseIt
+    {
+        
+
+        public static ChromeDriver driver;
+        private string m_baseURL = "http://en.wikipedia.org/wiki/Giant_panda";
+        private const int timeout = 10;
+        private const string m_expectedTitle = "Giant panda - Wikipedia";
+        private WebDriverWait wait;
+
+        [TestInitialize]
+        public void TestSetup()
+        {
+            driver = new ChromeDriver();
+            wait = new WebDriverWait(driver, TimeSpan.FromSeconds(timeout));
+        }
+
+        [TestMethod]
+        public void GoToURL()
+        {
+            driver.Navigate().GoToUrl(m_baseURL);
+            wait.Until(ExpectedConditions.TitleIs(m_expectedTitle));
+        }
+
+        [TestCleanup]
+        public void QuitBrowser()
+        {
+            driver.Quit();
+            driver = null;
+        }
+    }
+}
