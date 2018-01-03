6c5fe71dd8e40a9da746b0f9bde74a095349402c
KseniaStrizh <kseniya.blinnicheva@gmail.com>
2018-01-04 00:39:52 +0300

    finished with ex 11

--- CourseTasks/CourseTasks/Ex11.cs
+++ CourseTasks/CourseTasks/Ex11.cs
@@ -26,12 +26,17 @@ namespace CourseTasks
         #region repoElement
         public string Firstname => $".//input[@name='firstname']";
         public string Lastname => $".//input[@name='lastname']";
+        public string Company => $".//input[@name='company']";
+        public string TaxId => $".//input[@name='tax_id']";
         public string Address1 => $".//input[@name='address1']";
         public string PostCode => $".//input[@name='postcode']";
         public string City => $".//input[@name='city']";
-        public string Email => $".//input[@name='email']";
-        public string DesiredPassword => $".//input[@name='password']";
+        public string Email => $".//label[text()='Email']/..//input";
+        public string DesiredPassword => $".//label[text()='Desired Password']/..//input";
         public string ConfirmPassword => $".//input[@name='confirmed_password']";
+
+        public string SingInEmail = $".//div[@class='input-group']//input[@name='email']";
+        public string SingInPassword = $".//div[@class='input-group']//input[@name='password']";
         #endregion
 
         [TestInitialize]
@@ -46,7 +51,7 @@ namespace CourseTasks
         }
         private static string GenerateUnicName()
         {
-            string uniqueName = "KungFuPanda" + DateTime.Now.ToBinary() + "@gmail.com";
+            string uniqueName = "Test" + DateTime.Now.ToBinary() + "@gmail.com";
             return uniqueName;
         }
 
@@ -55,33 +60,31 @@ namespace CourseTasks
 
         {
             string email = GenerateUnicName();
+            driver.FindElement(By.XPath(TaxId)).SendKeys("TaxId");
+            driver.FindElement(By.XPath(Company)).SendKeys("Company");
             driver.FindElement(By.XPath(Firstname)).SendKeys("FirstName");
             driver.FindElement(By.XPath(Lastname)).SendKeys("LastName");
             driver.FindElement(By.XPath(Address1)).SendKeys("850 Ave");
             driver.FindElement(By.XPath(PostCode)).SendKeys("60654");
-            new SelectElement(driver.FindElement(By.CssSelector("select[name='country_code']"))).SelectByValue("US");
-          
 
+            new SelectElement(driver.FindElement(By.CssSelector("select[name='country_code']"))).SelectByValue("US");
+            Thread.Sleep(4000);
 
             driver.FindElement(By.XPath(Email)).SendKeys(email);
             
             driver.FindElement(By.XPath(DesiredPassword)).SendKeys("Password");
             driver.FindElement(By.XPath(ConfirmPassword)).SendKeys("Password");
- 
-           driver.FindElement(By.XPath(".//button[@name='create_account']")).Click();
-
 
             driver.FindElement(By.XPath(".//button[contains(@name,'create_account')]")).Click();
-
-
+            Thread.Sleep(10000);
             //Logout from the newly created account
             driver.FindElement(By.CssSelector("a[href *= 'logout']")).Click();
-
+            Thread.Sleep(10000);
             //Login again
-            driver.FindElement(By.CssSelector("input[name='email']")).SendKeys(email);
-            driver.FindElement(By.CssSelector("input[name='password']")).SendKeys("password");
+            driver.FindElement(By.XPath(SingInEmail)).SendKeys(email);
+            driver.FindElement(By.XPath(SingInPassword)).SendKeys("Password");
             driver.FindElement(By.CssSelector("button[name='login']")).Click();
-
+            Thread.Sleep(10000);
             //Logout again
             driver.FindElement(By.CssSelector("a[href *= 'logout']")).Click();
 
